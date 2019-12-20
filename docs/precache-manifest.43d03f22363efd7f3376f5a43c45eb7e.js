18effd07718b10954e74143cb8733ac6e834d852
angelabenavente <angela.b.almazan@gmail.com>
2019-12-20 16:53:32 +0100

    Add docs

---
+++ docs/precache-manifest.43d03f22363efd7f3376f5a43c45eb7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f231149bb38d0830f04f9d03b18767e9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2b308540320d2aada518",
+    "url": "./static/css/main.c2d90206.chunk.css"
+  },
+  {
+    "revision": "1c07d5de73f7457ec6c1",
+    "url": "./static/js/2.1e0de6a1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.1e0de6a1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2b308540320d2aada518",
+    "url": "./static/js/main.0698de5a.chunk.js"
+  },
+  {
+    "revision": "6fe7818c14c3616f861d",
+    "url": "./static/js/runtime-main.cf2d5a18.js"
+  }
+]);
